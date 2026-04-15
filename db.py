6c38e13ea1e1626cdca83f032a4c53e236e4928c
Shivarkar Praneel <praneel.private@gmail.com>
2026-04-15 13:40:15 +0530

    Delete db.py as node.js API was migrated

--- db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="india_location_db",
-        user="postgres",
-        password="password123"
-    )
